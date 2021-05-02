979244537c85724b1f96072a6af332fd1873db1d
Mitchell Flood <mitchflood1@gmail.com>
2021-05-02 15:55:31 -0600

    working on routes/making app function properly. Need to figure out how to make console 404s work

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const path = require("path");
+
+//make home page route
+
+//make exercise route
+
+//make stats route
+
+
+
+
+//boilerplate from mini project that is part of what the router variable uses to function properly 
+module.exports = router;
