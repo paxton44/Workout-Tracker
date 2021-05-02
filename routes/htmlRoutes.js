af447b508980ed1afc48db6414c5c9161a9a143d
Mitchell Flood <mitchflood1@gmail.com>
2021-05-02 16:16:13 -0600

    made homePage route

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,8 @@
-const router = require("express").Router();
-const path = require("path");
+const router = require('express').Router();
+const path = require('path');
 
-//make home page route
-
+//make home page route used mini project router.get to get the homepage because it is the first thing the user does before using the app. also used starwars final app to ref
+router.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
 //make exercise route
 
 //make stats route
